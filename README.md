32d711aa4ffd30ca1e78086e77c34eaead789991
itsumura-h <dumblepy@gmail.com>
2019-03-20 17:19:58 +0900

    init

---
+++ README.md
@@ -0,0 +1,92 @@
+Django_mod
+===
+
+Auto generating script of Django project with django-rest-framework, Orator, Jinja2
+
+## How to use?
+All you need is just making Python available and run following command
+```
+python -V
+> 3.6.8
+
+bash setup.sh
+```
+
+After that, you will be asked questions 3 times. whether use mysql or not, whether use test tool or not, whether use document support or not.
+If you want to say YES for all, you can run command with yes option as following
+
+```
+yes | bash setup.sh
+```
+If you use mysql, pymysql is going to be installed.  
+If you use test tool, pytest and faker are going to be installed.  
+If you use document support, sphinx and doc-cov are going to be installed.
+
+## Django-Rest-Framework
+https://www.django-rest-framework.org
+
+DRF is a useful tool to develop API
+
+## Orator
+https://orator-orm.com
+
+Orator is an Active Record ORM which is influenced by Laravel ORM
+
+## Jinja2
+http://jinja.pocoo.org/docs/2.10/
+
+Jinja2 is more useful template engin than default Django template system
+
+[pymysql](https://github.com/PyMySQL/PyMySQL)  
+[pytest](https://docs.pytest.org/en/latest/)  
+[faker](https://github.com/joke2k/faker)  
+[sphinx](http://www.sphinx-doc.org/en/stable/index.html)  
+[doc-cov](https://pypi.org/project/doc-cov/)
+
+## Sphinx setting
+If you want to generate documents in English, please replace 140 lines from 'ja' to 'en' in setup.sh
+
+```
+138 Author
+139 today
+140 ja
+141 .rst
+142 index
+```
+
+
+## !!Caution!!
+This shell and README.md will self-destruct after run setup.
+
+---
+
+これはdjango-rest-framework, Orator, Jinja2を含んだDjangoのプロジェクトを自動生成するスクリプトです。
+
+## 使い方
+Pythonを使えるようにして、setup.pyを実行するだけでOK！
+```
+python -V
+> 3.6.8
+
+bash setup.sh
+```
+
+対話型の質問が3回聞かれます。mysqlを使うかどうか、テストツールを使うかどうか、ドキュメントサポートを使うかどうか。  
+全てにyesで回答する場合は、yesのオプションをつけてコマンドを実行しましょう
+
+```
+yes | bash setup.sh
+```
+
+mysqlを使うと答えた場合は、pymysqlが、
+テストツールを使うと答えた場合は、pytestとfakerが
+ドキュメントサポートを使うと答えた場合はsphinxとdoc-covが追加でインストールされます。
+
+[pymysql](https://github.com/PyMySQL/PyMySQL)  
+[pytest](https://docs.pytest.org/en/latest/)  
+[faker](https://github.com/joke2k/faker)  
+[sphinx](http://www.sphinx-doc.org/ja/stable/index.html)  
+[doc-cov](http://cocodrips.hateblo.jp/entry/2019/02/06/234630)
+
+## !!注意!!
+なお、このバッチとREADME.mdは実行後自動的に消滅する。
---
+++ setup.sh
@@ -0,0 +1,223 @@
+#!/bin/bash -ex
+
+if [ ! -d project ]; then
+    git clone https://gitlab.com/dumblepy/django_mod.git
+    mv django_mod/* . && rm -fr django_mod
+elif [[ $1 = 'dev' ]]; then
+    rm project/app/fixture/sample_users.json
+    rm project/app/fixture/sample_posts.json
+elif [[ $1 = 'test' ]]; then
+    rm -fr project
+    git clone https://gitlab.com/dumblepy/django_mod.git
+    mv django_mod/* . && rm -fr django_mod
+else
+    git clone https://gitlab.com/dumblepy/django_mod.git
+    mv django_mod/* . && rm -fr django_mod
+fi
+
+#=============================================================================================
+
+python3 -m venv .venv
+source .venv/bin/activate
+
+pip3 install pipenv
+pkg='django djangorestframework django-filter orator Jinja2'
+
+mysqlflg='no'
+while :
+do
+    echo 'Do you use mysql? type y or n'
+    read input
+
+    case $input in
+        y)
+            pkg+=' pymysql'
+            mysqlflg='yes'
+            break
+            ;;
+        n)
+            break
+            ;;
+        *)
+            echo 'type y or n'
+            ;;
+    esac
+done
+
+while :
+do
+    echo 'Do you use test tool? type y or n'
+    read input
+
+    case $input in
+        y)
+            pkg+=' pytest faker'
+            break
+            ;;
+        n)
+            rm -fr project/app/fixture
+            break
+            ;;
+        *)
+            echo 'type y or n'
+            ;;
+    esac
+done
+
+while :
+do
+    echo 'Do you use document support? type y or n'
+    read input
+
+    case $input in
+        y)
+            pkg+=' sphinx doc-cov'
+            break
+            ;;
+        n)
+            rm -fr docs
+            break
+            ;;
+        *)
+            echo 'type y or n'
+            ;;
+    esac
+done
+
+#pipenv install pylint autopep8 django djangorestframework pymysql orator Jinja2 pytest faker eralchemy doc-cov
+if [[ $1 = 'dev' ]]; then
+    :
+elif [[ $1 = 'test' ]]; then
+    pipenv install $pkg
+    pipenv install pylint autopep8 --dev
+else
+    pipenv install $pkg
+    pipenv install pylint autopep8 --dev
+fi
+
+#=============================================================================================
+# mysql
+if [[ $1 = 'dev' ]] && [ $mysqlflg = 'yes' ]; then
+    :
+elif [[ $1 = 'test' ]] && [ $mysqlflg = 'yes' ]; then
+    sed -i -e "4i import pymysql" project/manage.py
+    sed -i -e "5i pymysql.install_as_MySQLdb()" project/manage.py
+    sed -i -e "6i\ " project/manage.py
+elif [ $mysqlflg = 'yes' ]; then
+    sed -i -e "4i import pymysql" project/manage.py
+    sed -i -e "5i pymysql.install_as_MySQLdb()" project/manage.py
+    sed -i -e "6i\ " project/manage.py
+fi
+
+
+#=============================================================================================
+if [[ $1 = 'dev' ]] && [ ! -d project/db.sqlite3 ]; then
+    python3 project/manage.py makemigrations
+    python3 project/manage.py migrate
+elif [[ $1 = 'test' ]]; then
+    rm project/db.sqlite3
+    python3 project/manage.py makemigrations
+    python3 project/manage.py migrate
+else
+    python3 project/manage.py makemigrations
+    python3 project/manage.py migrate
+fi
+
+#=============================================================================================
+
+# pytestが存在するときだけ実行
+if [ -f .venv/bin/pytest ]; then
+    python3 project/app/fixture/generator.py
+    python3 project/manage.py loaddata project/app/fixture/sample_users.json
+    python3 project/manage.py loaddata project/app/fixture/sample_posts.json
+else
+    rm -fr project/app/fixture
+fi
+
+#=============================================================================================
+
+# sphinxをセットアップ
+if [[ $1 = 'dev' ]]; then
+    :
+elif [[ $1 = 'test' ]] && [ -d docs ]; then
+    rm -fr docs
+else
+    :
+fi
+
+if [ ! -d docs ] && [ -f .venv/bin/sphinx-quickstart ]; then
+    mkdir docs
+
+sphinx-quickstart docs << EOF
+y
+_
+project
+Author
+today
+ja
+.rst
+index
+y
+y
+n
+y
+y
+n
+n
+n
+y
+n
+y
+n
+EOF
+
+fi
+# Separate source and build directories (y/n) [n]:
+# Name prefix for templates and static dir [_]:
+# Project name:
+# Author name(s):
+# Project release []:
+# Project language [en]:
+# Source file suffix [.rst]: .rst or .txt
+# Name of your master document (without suffix) [index]:
+# autodoc: automatically insert docstrings from modules (y/n) [n]:
+# doctest: automatically test code snippets in doctest blocks (y/n) [n]:
+# intersphinx: link between Sphinx documentation of different projects (y/n) [n]:
+# todo: write "todo" entries that can be shown or hidden on build (y/n) [n]:
+# coverage: checks for documentation coverage (y/n) [n]:
+# imgmath: include math, rendered as PNG or SVG images (y/n) [n]:
+# mathjax: include math, rendered in the browser by MathJax (y/n) [n]:
+# ifconfig: conditional inclusion of content based on config values (y/n) [n]:
+# viewcode: include links to the source code of documented Python objects (y/n) [n]:
+# githubpages: create .nojekyll file to publish the document on GitHub pages (y/n) [n]:
+# Create Makefile? (y/n) [y]:
+# Create Windows command file? (y/n) [y]:
+
+#sphinx-apidoc -F -o docs/source/ project/ -s html
+cd docs
+make html
+cd ../
+
+#=============================================================================================
+
+# .git削除 & git init
+if [[ $1 = 'dev' ]]; then
+    :
+elif [[ $1 = 'test' ]]; then
+    :
+else
+    if grep "Django_mod" README.md >/dev/null; then
+        rm -fr .git
+        git init
+
+    # README.mdを綺麗サッパリ
+    rm README.md
+cat << EOF > README.md
+your_project
+===
+EOF
+
+        #自分自身を削除
+        rm -- "$0"
+    fi
+fi
